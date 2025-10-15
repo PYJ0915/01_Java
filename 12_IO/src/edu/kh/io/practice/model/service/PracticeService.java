b6e114bb67345a2d79bb767f505a650248ddb265
PYJ0915 <qkrdbwls3542@gmail.com>
2025-10-15 17:49:40 +0900

    1015 수업 완료

--- 12_IO/src/edu/kh/io/practice/model/service/PracticeService.java
+++ 12_IO/src/edu/kh/io/practice/model/service/PracticeService.java
@@ -2,6 +2,8 @@ package edu.kh.io.practice.model.service;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
@@ -9,132 +11,168 @@ import java.io.InputStreamReader;
 public class PracticeService {
 
 	public void ex1() {
-		
-		/* 사용자로부터 텍스트를 입력받아 "output.txt"라는 파일에 저장하는 프로그램을 작성하세요.
-		  
-		   요구 사항: 1. 텍스트 파일에 내용을 쓰는 데 FileWriter 클래스를 사용하세요.
-					  2. "exit"을 입력하면 입력을 종료하고 파일을 닫도록 하세요.
+
+		/*
+		 * 사용자로부터 텍스트를 입력받아 "output.txt"라는 파일에 저장하는 프로그램을 작성하세요.
+		 * 
+		 * 요구 사항: 1. 텍스트 파일에 내용을 쓰는 데 FileWriter 클래스를 사용하세요. 2. "exit"을 입력하면 입력을 종료하고
+		 * 파일을 닫도록 하세요.
 		 */
-		
+
 		// 스트림 참조 변수 선언
 		BufferedReader br = null;
 		FileWriter fw = null;
-		
+
 		try {
-			
+
 			br = new BufferedReader(new InputStreamReader(System.in));
 			fw = new FileWriter("/io_practice/20251015/output.txt");
-			
-			while(true) {
-			
+
+			while (true) {
+
 				System.out.print("입력하고 싶은 내용을 입력하시오 (종료하고 싶으시다면 exit를 입력해주세요): ");
 				String input = br.readLine();
-				
-				if(input.equals("exit")) {
-					
+
+				if (input.equals("exit")) {
+
 					break;
-					
+
 				}
-				
+
 				fw.write(input + "\n");
-				
+
 			}
-			
+
 			fw.flush();
-			
+
 			System.out.println("출력 완료");
-			
+
 		} catch (Exception e) {
-			
-			e.printStackTrace();	
-			
-		} finally {
-			
-		try {
-			
-			if( br != null) br.close();
-			if( fw != null) fw.close();
-			
-		} catch (Exception e) {
-			
+
 			e.printStackTrace();
-			
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+				if (fw != null)
+					fw.close();
+
+			} catch (Exception e) {
+
+				e.printStackTrace();
+
+			}
+
 		}
-			
-		}
-		
+
 	}
-	
+
 	public void ex2() {
-		
+
 		File file = new File("C:/io_practice/20251015/input.txt");
-		
+
 		FileReader fr = null;
 		int value = 0;
-		StringBuilder sb  = new StringBuilder();
-		
+		StringBuilder sb = new StringBuilder();
+
 		try {
-			
-			if(!file.exists()) {
-				
+
+			if (!file.exists()) {
+
 				file.createNewFile();
-				
+
 			}
-			
+
 			fr = new FileReader("C:/io_practice/20251015/input.txt");
-			
-			while(true) {
-				
+
+			while (true) {
+
 				value = fr.read();
-				
-				if(value == -1) {
-					
+
+				if (value == -1) {
+
 					break;
-					
+
 				}
-				
-				sb.append((char)value);
-				
+
+				sb.append((char) value);
+
 			}
-			
-			if(sb.isEmpty()) {
+
+			if (sb.isEmpty()) {
 				System.out.println("파일이 비어있습니다.");
 				return;
 			}
-			
+
 			System.out.println(sb.toString());
-			
+
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
-			
+
 		} finally {
-			
+
 			try {
-				
-				if(fr != null) fr.close();
-				
+
+				if (fr != null)
+					fr.close();
+
 			} catch (Exception e) {
-				
+
 				e.printStackTrace();
-				
+
 			}
 		}
-		
+
 	}
-	
+
 	public void ex3() {
 		/*
 		 * 문제 3: 파일 복사하기
+		 * 
+		 * 문제: "source.txt"라는 파일을 "destination.txt"라는 새로운 파일로 복사하는 프로그램을 작성하세요.
+		 * 
+		 * 요구 사항:
+		 * 
+		 * FileInputStream과 FileOutputStream을 사용하여 파일을 복사하세요.
+		 * 
+		 * 복사 중 오류가 발생하면 예외를 처리하고, 복사 완료 후 "파일 복사가 완료되었습니다."라는 메시지를 출력하세요.
+		 */
 
-			문제: "source.txt"라는 파일을 "destination.txt"라는 새로운 파일로 복사하는 프로그램을 작성하세요.
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		File file = new File("C:/io_practice/20251015/source.txt");
 
-			요구 사항:
+		try {
 
-			FileInputStream과 FileOutputStream을 사용하여 파일을 복사하세요.
+			if (!file.exists()) {
 
-			복사 중 오류가 발생하면 예외를 처리하고, 복사 완료 후 "파일 복사가 완료되었습니다."라는 메시지를 출력하세요.*/
-		
+				System.out.println("읽어올 파일이 존재하지 않습니다.");
+			}
+
+			fis = new FileInputStream("C:/io_practice/20251015/source.txt");
+			fos = new FileOutputStream("C:/io_practice/20251015/destination.txt");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+			try {
+
+				if (fis != null)
+					fis.close();
+				if (fos != null)
+					fos.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
 	}
-	
+
 }
